9c5e0832b36eccddfd2dc6e135919885f0d48454
Optimixto <alegarmun@gmail.com>
2020-07-16 14:57:32 +0200

    Bug fix
    FIXED: Lost runs would not restart after failing.

---
+++ PopTheLock/Assets/LostRunCheck.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LostRunCheck : StateMachineBehaviour
+{
+    public GameData gameData;
+    private static readonly int LostRun = Animator.StringToHash("LostRun");
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(gameData.livesRemaining<=0)
+            animator.SetBool(LostRun, true);
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool(LostRun, false);
+    }
+}
